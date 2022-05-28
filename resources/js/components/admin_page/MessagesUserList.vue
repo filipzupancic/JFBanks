89e2bf61f051b21d5f70c0b9a7d2e70688373186
aleksander <aleksander.hrastic@ltfe.org>
2022-05-28 10:45:15 +0200

    dashboard admin edit

--- resources/js/components/admin_page/MessagesUserList.vue
+++ resources/js/components/admin_page/MessagesUserList.vue
@@ -3,11 +3,7 @@
   <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
-        <h1 class="text-xl font-semibold text-gray-900">Uporabniki</h1>
-        <p class="mt-2 text-sm text-gray-700">Uporabniki, ki želijo kredit</p>
-      </div>
-      <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <button type="button" @click='oy()' class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
+<h1 class="text-xl font-semibold text-gray-900">Sporočila</h1>
       </div>
     </div>
     <div class="mt-8 flex flex-col">
@@ -18,24 +14,16 @@
               <thead class="bg-gray-50">
                 <tr>
                   <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Ime in priimek</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Starost</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Tip kredita</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Kreditna sposobnost</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
-                    <span class="sr-only">Več podrobnosti</span>
-                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Zadnje sporočilo</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">Možnost</th>
                 </tr>
               </thead>
               <tbody class="divide-y divide-gray-200 bg-white">
-                <tr v-for="person in people" :key="person.email" >
+                <tr v-for="person in people" :key="person.email" class='hover:bg-gray-300' >
                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ person.name }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.age}}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.type }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.credithworthy }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.message}}</td>
                   <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                      >Edit<span class="sr-only">, {{ person.name }}</span></a
-                    >
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Kontaktiraj<span class="sr-only"></span></a>
                   </td>
                 </tr>
               </tbody>
@@ -48,14 +36,12 @@
 </template>
 
 <script setup>
-function oy(){
+function clickMsg(){
   console.log("oy")
 }
 const people = [
-  { name: 'Pehta', age: '55',credithworthy:'37%', type:'Poljubno',email: 'pehta@example.com'},
-  { name: 'Kekec', age: '30',credithworthy:'99%', type:'Živinoreja',email: 'kekec@example.com'},
-  { name: 'Bedanc', age: '40',credithworthy:'79%', type:'Potrošniški',email: 'bedanc@example.com'},
-  { name: 'Mojca', age: '40',credithworthy:'89%', type:'Potrošniški',email: 'bedanc@example.com'},
+  { name: 'Bogdan', message:'Pozdravljeni, kako lahko pridobim kred...' ,email: 'pehta@example.com'},
+  { name: 'Živa', message:'Kje lahko pridobim potrdilo o nepre...' ,email: 'kekec@example.com'},
   // More people...
 ]
 </script>
---
+++ resources/js/components/admin_page/OffersList.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1 class="text-xl font-semibold text-gray-900">Aktivne ponudbe</h1>
+      </div>
+      <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        <button type="button" @click='dodajOffer' class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Dodaj ponudbo</button>
+      </div>
+    </div>
+    <div class="mt-8 flex flex-col">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle">
+          <div class="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Ime ponudbe</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Opis ponudbe</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Obrestna mera</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">Uredi</th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <tr v-for="person in people" :key="person.email" class='hover:bg-gray-300'>
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ person.name }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.vsebina}}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.obrestnamera}}</td>
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Odstrani/Spremeni<span class="sr-only"></span></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+function dodajOffer(){
+  people.value.push({ name: 'Dodatno zavarovanje stanovanja', vsebina:'Zavarovanje stanovanja',obrestnamera:'2.7%' ,email: 'pehta@example.com'})
+}
+const people = ref([
+  { name: 'Dodatno zavarovanje vrta', vsebina:'Zavarovanje vrta',obrestnamera:'2.2%' ,email: 'pehta@example.com'},
+  { name: 'Dodatno zavarovanje živali', vsebina:'Zavarovanje stanovanja',obrestnamera:'2.1%' ,email: 'pehta@example.com'},
+  // More people...
+])
+</script>
